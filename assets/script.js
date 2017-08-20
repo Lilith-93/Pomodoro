f071e6b01d95f30fc0f46f45ff6687711d2e42c6
Lilith-93 <deborah.delcourt@free.fr>
2017-08-20 16:56:41 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var time = 0; //en sec
 var inter = 0;
 var min = 0;
 var sec = 0;
-
+	
 //FONCTION POUR AFFICHER LE TEMPS DE TRAVAIL EN SECONDES AU CLICK DU BOUTON
 	$("#boulot").click(function(){
 		time = 1500; //25'
@@ -37,6 +37,7 @@ var sec = 0;
 			time--;
 			sec--;
 			$("#temps").text(min+'min '+sec+'sec.');
+			$("title").text(min+'min '+sec+'sec.');
 			if(time<=0){
 				document.getElementById('alarm').play();
 				clearInterval(inter);
