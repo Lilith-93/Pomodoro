5a7fdef71567c46038b23c1de2e1808905b4a9d7
Lilith-93 <deborah.delcourt@free.fr>
2017-08-21 09:48:54 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var time = 0; //en sec
 var inter = 0;
 var min = 0;
 var sec = 0;
-	
+
 //FONCTION POUR AFFICHER LE TEMPS DE TRAVAIL EN SECONDES AU CLICK DU BOUTON
 	$("#boulot").click(function(){
 		time = 1500; //25'
@@ -34,22 +34,19 @@ var sec = 0;
 	});	
 	//FONCTION POUR DECREMENTER LE TIMER ET CLEAR L'INTERVAL
 		function count(){
-			if(time<=0){
-				document.getElementById('alarm').play();
-				clearInterval(inter);
-				alert('Ton temps est écoulé !');
-			}
-			time--;			
+			time--;
 			sec--;
 			if(sec<0){
 				min--;
 				sec = 59;
 			}
 			$("#temps").text(min+'min '+sec+'sec.');
-			$("title").text(min+"m"+sec+"s");
-			
-			
-			
+			$("title").text(min+'min '+sec+'sec.');
+			if(time<=0){
+				document.getElementById('alarm').play();
+				clearInterval(inter);
+				alert('Ton temps est écoulé !');
+			}
 		}
 //FONCTION POUR METTRE EN PAUSE AU CLICK DU BOUTON
 	$("#pause").click(function(){
